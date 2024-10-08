bf67b5ab0624933206bbfed29223be2770584c42
dmiccoli18 <apmiccoli018@gmail.com>
2024-10-08 00:45:01 -0400

    Updated despacer.sh with a description

--- despacer.sh
+++ despacer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This code changes spaces to underscores in all files in a given directory. this functioned as a way to clean my files of spaces before organizing or moving
 echo "Choose a directory: "
 read DIR
 cd $DIR
